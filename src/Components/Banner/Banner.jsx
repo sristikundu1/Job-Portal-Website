265f9cd15e02b0dd21487f0fc4a694287a2aadff
sristikundu1 <nidhukundu1234@gmail.com>
2023-11-08 23:25:26 +0600

    make banner site responsive

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
                 <NavBar></NavBar>
             </div>
             <div className="flex flex-col lg:grid grid-cols-3 p-7">
-                <div className="col-span-2 pl-28 space-y-7 py-4">
+                <div className="lg:col-span-2 pl-28 space-y-7 py-4">
                     <h2 className="font-bold text-6xl">Your Path to Success Starts Here</h2>
                     <p className="font-medium text-xl">Welcome to DreamCatalyst, where job seekers and employers come b together. <br />Find the perfect job or ideal candidate with ease. Post, search, and apply – your future begins now.</p>
                     <input className="w-96 h-11 p-4 rounded" type="text" placeholder="Search for jobs......" name="" id="" />
